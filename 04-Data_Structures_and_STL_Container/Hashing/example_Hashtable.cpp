1520dd4f4e3461af0efdd8490d32192bf804c309
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-12-11 22:32:45 +0530

    Create example_Hashtable.cpp

---
+++ 04-Data_Structures_and_STL_Container/Hashing/example_Hashtable.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+unordered_map<string,int>
+menu;
+
+//Insert key:value pairs inside the hashtable o(1) operation
+menu["maggi"]=15;
+menu["coldrink"]=20;
+menu["dosa"]=99;
+menu["pizza"]=499;
+menu["Burger"]=150;
+
+//update price
+menu["dosa"]=(1+0.1)*menu["dosa"];
+
+//Deletion
+menu.erase("dosa");
+
+//search inside a collection of key value pairs (stored inside a hash table)
+string item;
+cin>>item;
+
+if(menu.count(item)==0){
+	cout<<item<<" is not available";
+}
+else{
+	cout<<item<<" is available, and its cost is "<<menu[item]<<endl;
+}
+
+
+//We can iterate over all the key-values pair that are present
+
+for(pair<string,int> item :menu){
+	cout<<item.first<<" - "<<item.second<<endl;
+}
+
+cout<<endl;
+
+for(auto itr : menu){
+	cout<<itr.first<<" - "<<itr.second<<endl;
+}
+
+return 0;
+} 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Input--
+// pizza
+// Output--
+// pizza is available, and its cost is 499
+// Burger - 150
+// pizza - 499
+// coldrink - 20
+// maggi - 15
+
+// Burger - 150
+// pizza - 499
+// coldrink - 20
+// maggi - 15
