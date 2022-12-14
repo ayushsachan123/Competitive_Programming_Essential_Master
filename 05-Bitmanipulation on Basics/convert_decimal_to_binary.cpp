198527e21073e0732b23e0fa06fc4ef24c9dff8c
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-12-14 22:44:50 +0530

    Create convert_decimal_to_binary.cpp

---
+++ 05-Bitmanipulation on Basics/convert_decimal_to_binary.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int convertToBinary(int n){
+	int ans=0;
+	int p = 1;
+while(n>0){
+	int last_bit = (n&1);
+	ans+=p*last_bit;
+    p=p*10;
+    n= n>>1;
+}
+return ans;
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+int n;
+cin>>n;
+cout<<convertToBinary(n)<<endl;
+return 0;
+} 
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// 5
+// 101
