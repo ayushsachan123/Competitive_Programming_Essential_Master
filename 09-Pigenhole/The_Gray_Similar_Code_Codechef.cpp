e868832be87a824035df33a06e9f1669fcfe8e8d
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-12-24 22:52:33 +0530

    Create The_Gray_Similar_Code_Codechef.cpp

---
+++ 09-Pigenhole/The_Gray_Similar_Code_Codechef.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> a;
+	for(int i=0; i<n; i++) {
+	    int x;
+	    cin >> x;
+	    a.push_back(x);
+	}
+	
+    if(n>=130) {
+        cout << "Yes";
+        return;
+    }
+    else {
+        sort(a.begin(),a.end());
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    int b=a[i]^a[j]^a[k];
+                    auto ptr=a.begin();
+                    advance(ptr,k+1);
+                     if(find(ptr, a.end(),b)!=a.end())
+                    {
+                        cout<<"Yes";
+                       return;
+                    }
+                }
+            }
+        }
+        cout << "No";
+        return;
+    }
+}
+
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	solve();
+
+	return 0;
+}
+
+//Input--
+// 5
+// 1 0 2 3 7
+// Output--
+// Yes
