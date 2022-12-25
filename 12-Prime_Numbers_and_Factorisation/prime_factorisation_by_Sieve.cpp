8c7ae493c51efd396bd2121fb7c048334b110004
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2022-12-25 21:43:22 +0530

    Create prime_factorisation_by_Sieve.cpp

---
+++ 12-Prime_Numbers_and_Factorisation/prime_factorisation_by_Sieve.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define N 1000000
+#define ll long long
+using namespace std;
+
+void primeSieve(vector<int> &sieve){
+
+ //Mark 1 and 0 as not prime
+   sieve[1]=sieve[0] = 0;
+
+    //start from 2 and mark all multiples of ith number (prime) as not prime
+	for(ll i=2;i<=N;i++){
+      if(sieve[i]){ //if ith number is not mark then it is the prime no
+         for(ll j = i*i;j<=N;j=j+i){
+         	//marking j as not prime
+            sieve[j]=0;
+         }
+      }
+	}
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+vector<int> sieve(N,1); //Fill Constructor
+ primeSieve(sieve);
+
+ for(int i=0;i<=100;i++){
+ 	if(sieve[i]==1){
+ 		cout<<i<<" ";
+ 	}
+ }
+return 0;
+
+} 
+
+//Output--
+// 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 9 
